995a7f5bc5c519516a3e89f715718bf8a569aaf4
Jarosław Skrzypek <skrzypek.jarek@gmail.com>
2017-01-09 23:06:49 +0100

    exercises 8-12

---
+++ README.md
@@ -0,0 +1,11 @@
+ndarray 100 exercises
+=====================
+
+Reimplementation of numpy 100 exercises in Rust & ndarray
+
+Based on:
+---------
+
+http://www.labri.fr/perso/nrougier/teaching/numpy.100/
+
+https://github.com/termoshtt/ndarray100/blob/master/src/main.rs
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
-#[macro_use]
+#[macro_use(s)]
 extern crate ndarray;
+extern crate ndarray_rand;
+extern crate rand;
+
 use ndarray::*;
+use ndarray_rand::RandomExt;
 
 fn main() {
     println!("Please run `cargo test` to run examples");
 }
 
 /*
- * 100 Excersises for Numpy:
+ * 100 Exercises for Numpy:
  * http://www.labri.fr/perso/nrougier/teaching/numpy.100/
  */
 
 #[test]
-fn excersise3() {
+fn exercise3() {
     let arr = Array1::<f64>::zeros((10));
     assert_eq!(arr.len(), 10);
     assert_eq!(arr.dim(), (10));
 }
 
 #[test]
-fn excersise5() {
+fn exercise5() {
     let mut arr = Array1::<f64>::zeros((10));
     arr[4] = 1.0;
     assert_eq!(arr[4], 1.0);
@@ -33,22 +37,81 @@ fn exercise6() {
     assert_eq!(arr[39], 49.0);
 }
 
+// TODO: add more solutions for exercise 7
 #[test]
 fn exercise7() {
-    // TODO: add more solutions
-    let arr: Array1<f64> = Array::range(0.0, 50.0, 1.0);
+    let arr: Array1<f64> = ndarray::Array::range(0.0, 50.0, 1.0);
     let reverse = arr.slice(s!(..;-1));
     assert_eq!(reverse.len(), 50);
     assert_eq!(reverse[0], 49.0);
     assert_eq!(reverse[49], 0.0);
+}
+#[test]
+fn exercise7b() {
+    let arr: Array1<f64> = Array::linspace(49.0, 0.0, 50);
+    assert_eq!(arr.len(), 50);
+    assert_eq!(arr[0], 49.0);
+    assert_eq!(arr[49], 0.0);
 }
 
 #[test]
-fn exercise7() {
-    // TODO: add more solutions
-    let arr: Array1<f64> = Array::range(0.0, 50.0, 1.0);
-    let reverse = arr.slice(s!(..;-1));
-    assert_eq!(reverse.len(), 50);
-    assert_eq!(reverse[0], 49.0);
-    assert_eq!(reverse[49], 0.0);
+fn exercise8() {
+    // TODO: can we do otherwise?
+    let matrix: Array2<f64> = Array::range(0.0, 9.0, 1.0).into_shape((3,3)).unwrap();
+    assert_eq!(matrix.dim(), (3,3));
+    assert_eq!(matrix[(0,0)], 0.0);
+    assert_eq!(matrix[(1,0)], 3.0);
+    assert_eq!(matrix[(2,2)], 8.0);
 }
+
+#[test]
+fn exercise9() {
+ let arr = Array::from_vec(vec![1,2,0,0,4,0]);
+ let indices: Vec<usize> = arr.indexed_iter().filter(|&(_,x)| { *x != 0 }).map(|(i,_)| i).collect();
+ assert_eq!(indices, vec![0,1,4]);
+}
+
+#[test]
+fn exercise9b() {
+ let arr = Array::from_vec(vec![1,2,0,0,4,0]);
+ let indices: Vec<usize> = arr.indexed_iter().filter_map(|(i,x)| {
+     if *x != 0 { return Some(i); } else { return None }
+ }).collect();
+ assert_eq!(indices, vec![0,1,4]);
+}
+
+#[test]
+fn exercise10() {
+    let arr: Array2<f64> = Array::eye(3);
+    assert_eq!(arr[(0,0)], 1.0);
+    assert_eq!(arr[(1,1)], 1.0);
+    assert_eq!(arr[(2,2)], 1.0);
+    assert_eq!(arr[(1,0)], 0.0);
+}
+
+#[test]
+fn exercise11() {
+    // Watch out: if you get error:
+    // "trait `rand::distributions::Normal: rand::distributions::IndependentSample<_>` not satisfied"
+    // make sure your Cargo.toml includes crate rand=0.3
+    let arr = Array::random((3, 3, 3 as usize),
+                            rand::distributions::normal::Normal::new(0.0, 1.0));
+
+    assert_eq!(arr.len(), 27);
+    assert_ne!(arr[(0,0,0)], arr[(2,2,2)]);
+}
+
+#[test]
+fn exercise12() {
+    // Watch out: if you get error:
+    // "trait `rand::distributions::Normal: rand::distributions::IndependentSample<_>` not satisfied"
+    // make sure your Cargo.toml includes crate rand=0.3
+    let arr = Array::random((10, 10, 3 as usize),
+                            rand::distributions::normal::Normal::new(0.0, 5.0));
+    let inf = 0.0/0.0;
+    let min = arr.fold(inf,  |r, x| { x.min(r) });
+    let max = arr.fold(-inf, |r, x| { x.max(r) });
+    assert!(min < max);
+}
+
+
